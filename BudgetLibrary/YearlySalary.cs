87b2fac39334c2c9310eefcbb90d9c74d78a4828
MasonPhillip <97992332+MasonPhillip@users.noreply.github.com>
2025-02-16 12:16:37 -0600

    Added a yearly salary object

---
+++ BudgetLibrary/YearlySalary.cs
@@ -0,0 +1,36 @@
+﻿using BudgetLibraryTests;
+
+namespace BudgetLibrary
+{
+    public class YearlySalary
+    {
+        /// <summary>
+        /// The Yearly Salary Amount
+        /// </summary>
+        public decimal Salary { get; set; }
+
+        /// <summary>
+        /// Creates a yearly salary
+        /// </summary>
+        /// <param name="salary">the yearly payment amount</param>
+        public YearlySalary(decimal salary) {
+            if (salary < 0 || salary > 1000000)
+            {
+                throw new ArgumentException("Salary Can't be negative");
+            }
+            Salary = salary;
+        }
+
+        public decimal GetPayment(PaymentPeriod frequency)
+        {
+            if (frequency == PaymentPeriod.Semimonthly)
+            {
+                return Salary / 24;
+            }
+            else
+            {
+                return Salary / 52;
+            }
+        }
+    }
+}
---
+++ BudgetLibraryTests/YearlySalaryTests.cs
@@ -0,0 +1,57 @@
+﻿using BudgetLibrary;
+
+namespace BudgetLibraryTests
+{
+    public class YearlySalaryTests
+    {
+        [Theory]
+        [InlineData(-1000)]
+        [InlineData(-100000)]
+        public void SalaryCantBeNegative(decimal salary)
+        {
+            Assert.Throws<ArgumentException>(() => { YearlySalary s = new YearlySalary(salary); });
+        }
+
+        [Theory]
+        [InlineData(1000)]
+        [InlineData(10000)]
+        [InlineData(70000)]
+        [InlineData(100000)]
+        [InlineData(1000000)]
+        public void CanSetSalary(decimal salary)
+        {
+            YearlySalary s = new YearlySalary(salary);
+            Assert.Equal(salary, s.Salary);
+        }
+
+        [Theory]
+        [InlineData(1000001)]
+        [InlineData(100000000000)]
+        public void MaxSalaryIsOneMillion(decimal salary)
+        {
+            Assert.Throws<ArgumentException>(() => { YearlySalary s = new YearlySalary(salary); });
+        }
+
+        [Theory]
+        [InlineData(80000, 3333.33)]
+        [InlineData(100000, 4166.67)]
+        [InlineData(1000000, 41666.67)]
+        public void GetSemimonthlypay(decimal salary, decimal expected)
+        {
+            YearlySalary s = new YearlySalary(salary);
+            Assert.Equal(expected, s.GetPayment(PaymentPeriod.Semimonthly), 2);
+        }
+
+        [Theory]
+        [InlineData(80000, 1538.46)]
+        [InlineData(100000, 1923.08)]
+        [InlineData(1000000, 19230.77)]
+        public void GetWeeklypay(decimal salary, decimal expected)
+        {
+            YearlySalary s = new YearlySalary(salary);
+            Assert.Equal(expected, s.GetPayment(PaymentPeriod.Weekly), 2);
+        }
+
+
+    }
+}
