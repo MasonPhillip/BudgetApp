9dd72aaf3b59a59e31ecb4f4c8364176ceaec63d
MasonPhillip <97992332+MasonPhillip@users.noreply.github.com>
2025-02-26 21:30:57 -0600

    Added present value calculator

---
+++ BudgetLibrary/TVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetLibrary
+{
+    public class TVM
+    {
+        public static decimal PresentValue(double n, decimal rate, decimal fv)
+        {
+            decimal denominator = (decimal)Math.Pow((double)(1+rate), (double)n);
+            return -fv/denominator;
+        }
+
+    }
+}
---
+++ BudgetLibraryTests/TVMTests.cs
@@ -0,0 +1,18 @@
+﻿using BudgetLibrary;
+
+namespace BudgetLibraryTests
+{
+    public class TVMTests
+    {
+
+        [Theory]
+        [InlineData(10, 0.05, 10000, -6139.13253540759)]
+        [InlineData(20, 0.1, -36000, 5351.17060886916)]
+        public void CanCalulatePV(double n, decimal rate, decimal fv, decimal expected)
+        {
+            decimal result = TVM.PresentValue(n, rate, fv);
+            Assert.Equal(result, expected, 2);
+            
+        }
+    }
+}
